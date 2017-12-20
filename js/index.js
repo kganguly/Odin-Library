dff0c6f73d0566e13824b279630ef34345955c89
Kaushik Ganguly <kganguly@gmail.com>
2017-12-20 12:24:07 -0500

    Add localstorage.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     setListeners();
-    addBookToLibrary(new Book("Wild", "Steve", 10, false));
-    addBookToLibrary(new Book("Humble", "Bob", 99, false));
-    addBookToLibrary(new Book("Lost", "Alice", 345, true));
+    loadData();
     render();
 });
 
@@ -17,13 +15,40 @@ class Book {
     }
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary(book, doStoreData = true) {
     myLibrary.push(book);
+    if (doStoreData) storeData();
 }
 
 function removeBookFromLibrary(index) {
     myLibrary.splice(index, 1);
-    render();
+    storeData();
+}
+
+function toggleBookReadInLibrary(index) {
+    const book = myLibrary[index];
+    book.read = !book.read;
+    storeData();
+}
+
+function storeData() {
+    localStorage.setItem("library", JSON.stringify(myLibrary));
+    console.log(`STORE: ${JSON.stringify(myLibrary)}`);
+}
+
+function loadData() {
+    const data = localStorage.getItem("library");
+    console.log(`DATA: ${data}`);
+    if (!data) {
+        addBookToLibrary(new Book("Wild", "Steve", 10, false));
+        addBookToLibrary(new Book("Humble", "Bob", 99, false));
+        addBookToLibrary(new Book("Lost", "Alice", 345, true));
+    } else {
+        const jsonArray = JSON.parse(data);
+        for (let bookJson of jsonArray) {
+            addBookToLibrary(new Book(bookJson.title, bookJson.author, bookJson.numPages, bookJson.read), false);
+        }
+    }
 }
 
 function render() {
@@ -52,13 +77,19 @@ function render() {
         div.classList.add("readmark");
         if (book.read) div.classList.add("read");
         div.innerText = "\u2713";
-        div.addEventListener("click", () => toggleRead(i));
+        div.addEventListener("click", () => {
+            toggleBookReadInLibrary(i);
+            event.target.classList.toggle("read");
+        });
         li.appendChild(div);
 
         let button = document.createElement("button");
         button.textContent = "X";
         button.classList.add("remove-button");
-        button.addEventListener("click", () => removeBookFromLibrary(i));
+        button.addEventListener("click", () => {
+            removeBookFromLibrary(i);
+            render();
+        });
         li.appendChild(button);
         bookNodes.appendChild(li);
     }
@@ -70,7 +101,7 @@ function setListeners() {
     const addBtn = document.getElementById("newBookBtn");
     addBtn.addEventListener("click", showModal);
     window.addEventListener("click", windowOnClick);
-    const form = document.getElementsByTagName("form")[0];
+    const form = document.forms[0];
     form.addEventListener("submit", processForm);
 }
 
@@ -97,10 +128,4 @@ function showModal() {
 function hideModal() {
     const modal = document.getElementsByClassName("modal")[0];
     modal.classList.remove("show-modal");
-}
-
-function toggleRead(index) {
-    const book = myLibrary[index];
-    book.read = !book.read;
-    event.target.classList.toggle("read");
 }
